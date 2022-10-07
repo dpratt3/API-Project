db4cd64a4b17f59bd4295fd47462b4bd78357435
David Pratt <david@midnightmechanism.com>
2022-10-06 22:25:42 -0500

    Add User authentication Express middleware

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -8,23 +8,30 @@ const { restoreUser } = require("../../utils/auth.js");
 // router.use(restoreUser);
 
 // GET /api/set-token-cookie
-const { setTokenCookie } = require("../../utils/auth.js");
-const { User } = require("../../db/models");
-router.get("/set-token-cookie", async (_req, res) => {
-  const user = await User.findOne({
-    where: {
-      username: "Demo-lition",
-    },
-  });
-  console.log("-----------------------", user);
-  setTokenCookie(res, user);
-  return res.json({ user });
-});
+// const { setTokenCookie } = require("../../utils/auth.js");
+// const { User } = require("../../db/models");
+// router.get("/set-token-cookie", async (_req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: "Demo-lition",
+//     },
+//   });
+//   console.log("-----------------------", user);
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// });
 
+// router.use(restoreUser);
+
+// router.get("/restore-user", (req, res) => {
+//   return res.json(req.user);
+// });
+
+// // GET /api/require-auth
+// const { requireAuth } = require("../../utils/auth.js");
+// router.get("/require-auth", requireAuth, (req, res) => {
+//   return res.json(req.user);
+// });
 router.use(restoreUser);
-
-router.get("/restore-user", (req, res) => {
-  return res.json(req.user);
-});
 
 module.exports = router;
